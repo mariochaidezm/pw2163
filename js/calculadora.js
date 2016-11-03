78395c2686ce3a9193c59e0b872a591913c82dd9
mariochaidezm <mario.chaidez.m@gmail.com>
2016-11-03 12:00:26 -0700

    continuacion calculadora

---
+++ js/calculadora.js
@@ -0,0 +1,34 @@
+//variable global.
+var operador = "";
+
+
+function operadores(ope) {
+	operador = ope;
+}
+function numeros(num)
+{
+	if(operador == "") //Escribir en el operando1
+		//guardamos el valor del operando 1
+		var valor = document.calculadora.operando1.value;
+		if(valor=="0")
+		{
+			document.calculadora.operando1.value = "";
+		}
+	document.calculadora.operando1.value = document.calculadora.operando1.value + num;
+	else//escribir en el operando2
+		{
+		//guardamos el valor del operando 2
+		var valor = document.calculadora.operando2.value;
+		if(valor=="0")
+		{
+			document.calculadora.operando2.value = "";
+		}
+	document.calculadora.operando2.value = document.calculadora.operando2.value + num;
+}
+function igual() {
+	// body...
+	var valor1 = document.calculadora.operando1.value;
+	var valor2 = document.calculadora.operando2.value;
+	var valor1 = 0;
+	document.calculadora.resultado = eval(valor1+operador+valor2);
+}
