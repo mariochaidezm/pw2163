352e60df461de3a3d8053c1a4557ffb92afb01ba
mariochaidezm <mario.chaidez.m@gmail.com>
2016-11-17 11:52:22 -0700

    17 noviembre continuacion

--- js/eventos.js
+++ js/eventos.js
@@ -7,8 +7,11 @@ var inicio = function() //Main
   url: 'https://randomuser.me/api/',
   dataType: 'json',
   success: function(data) {
+    $("#imgfoto").show("slow");
+    $("#txtnombre").show("slow");
   	$("#txtnombre").val(data.results[0].name.first+" "+data.results[0].name.last)
   	$("#imgfoto").attr("src",data.results[0].picture.large)
+    $("#miArticle").html("Texto");
     console.log(data.results[0].name.first+" "+data.results[0].name.last);
   }
 });
